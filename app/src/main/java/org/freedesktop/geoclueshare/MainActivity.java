58c86525f2d2566fc7b59a808dc4e2b66de50554
Ankit <ankitstarski@gmail.com>
2015-07-05 19:22:10 +0530

    Initialize Android project
    Initialize an empty Android Studio project for GeoclueShare.

---
+++ app/src/main/java/org/freedesktop/geoclueshare/MainActivity.java
@@ -0,0 +1,35 @@
+package org.freedesktop.geoclueshare;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/*
+ * Copyright (C) 2015 Ankit (Verma)
+ *
+ * GeoclueShare is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * GeoclueShare is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GeoclueShare; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author: Ankit (Verma) <ankitstarski@gmail.com>
+ */
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle(R.string.main_activity_label);
+    }
+}
