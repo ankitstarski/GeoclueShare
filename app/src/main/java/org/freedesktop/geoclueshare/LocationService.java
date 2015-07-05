f1f895bd6dc7dc830ec6470df9b2c69996c20ca9
Ankit <ankitstarski@gmail.com>
2015-07-06 01:26:17 +0530

    Add NmeaListener
    LocationService will also act as NmeaListener and will be used to fetch
    NMEA sentences from the GPS.

--- app/src/main/java/org/freedesktop/geoclueshare/LocationService.java
+++ app/src/main/java/org/freedesktop/geoclueshare/LocationService.java
@@ -3,6 +3,11 @@ package org.freedesktop.geoclueshare;
 import android.app.Service;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -34,13 +39,34 @@ import android.util.Log;
  * </p>
  * FIXME: It's just a dummy class yet. Add functionality.
  */
-public class LocationService extends Service {
+public class LocationService extends Service implements LocationListener, GpsStatus.NmeaListener {
 
     private static final String TAG = "LocationService";
+    private LocationManager locationManager;
+
+    /**
+     * The minimum time between updates in milliseconds.
+     */
+    private static final long MIN_TIME_BW_UPDATES_GPS = 1000;
+
+    /**
+     * The minimum distance to change Updates in meters.
+     */
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "Service started");
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                MIN_TIME_BW_UPDATES_GPS,
+                MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                this);
+        locationManager.addNmeaListener(this);
+
         return Service.START_STICKY;
     }
 
@@ -58,6 +84,38 @@ public class LocationService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        locationManager.removeUpdates(this);
+        locationManager.removeNmeaListener(this);
+
         Log.d(TAG, "Service destroyed");
+    }
+
+    @Override
+    public void onNmeaReceived(long timestamp, String nmea) {
+        String nmeaTimestamp = nmea.split(",")[1];
+        if (nmea.startsWith("$GPGGA") && nmeaTimestamp.matches("[0-9]{6}")) {
+            Log.d(TAG, "GGA Sentence: " + nmea);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
